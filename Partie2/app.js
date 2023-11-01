871659243573900b4b5259ba4501c441312a0f0a
Mourtallasyll <serignemourtallasyll86@gmail.com>
2023-11-01 18:02:33 +0000

    Partie 2

---
+++ Partie2/app.js
@@ -0,0 +1,20 @@
+//Value types
+//Number, String, Boolean...
+let x=10;
+
+let y = x;
+
+y= 20;
+
+console.log(x , y);
+
+//Ref Types
+//[Object, Array]
+
+let a = {valeur: 10}
+
+let b = a;
+
+a.valeur = 20;
+
+console.log(a , b)
